b6df4a38fd0fe6d30c27c862017019bd13a810aa
Tomas Baca <klaxalk@gmail.com>
2024-10-25 10:44:06 +0200

    fix build

--- .ci/build_docker.sh
+++ .ci/build_docker.sh
@@ -9,15 +9,15 @@ trap 'echo "$0: \"${last_command}\" command failed with exit code $?' ERR
 MY_PATH=`dirname "$0"`
 MY_PATH=`( cd "$MY_PATH" && pwd )`
 
-cd $MY_PATH
+cd $MY_PATH/..
 
 docker login --username klaxalk --password $TOKEN
 
-source ../VERSION.sh
+source ./VERSION.sh
 
 NAME=dogtail
 REGISTRY=klaxalk
 
 docker buildx create --name container --driver=docker-container --use
 
-docker buildx build ../ --file Dockerfile --tag $REGISTRY/$NAME:latest --tag $REGISTRY/$NAME:$VERSION --platform=linux/amd64,linux/arm64 --push 
+docker buildx build . --file Dockerfile --tag $REGISTRY/$NAME:latest --tag $REGISTRY/$NAME:$VERSION --platform=linux/amd64,linux/arm64 --push 
